cddd34abae4658661b67601407ed46eee3ec3f95
Ajayrx <ajay0i0know@gmail.com>
2025-03-16 17:48:17 +0530

    git-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Ncrypt-website/" : "/" , // Replace with your actual GitHub repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
